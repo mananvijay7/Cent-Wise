3eeb698f0074af904dd3bffdf6b8147c6244c58f
Kshiti Dangore <dangore.k@northeastern.edu>
2023-12-11 16:25:02 -0500

    added second chart

--- frontend/components/ChartVisuals/ChartModal.tsx
+++ frontend/components/ChartVisuals/ChartModal.tsx
@@ -14,10 +14,16 @@ const ChartModal: React.FC<ChartModalProps> = ({ isVisible, onClose }) => {
   console.log('isVisible:', isVisible);
 
   const [chartData, setChartData] = useState({
-    datasets: [{
-      data: [10, 20, 30],
-      backgroundColor: ['#00CE67', '#ffa500', '#FFE347'],   //Green,Orange,Yellow
-    }],
+    datasets: [
+      {
+        data: [10, 20, 30],
+        backgroundColor: ['#00CE67', '#ffa500', '#FFE347'],   // Green, Orange, Yellow
+      },
+      {
+        data: [5, 15, 25],
+        backgroundColor: ['#D81E5B', '#731DD8', '#FAEDCA'], // Pink, Violet, White
+      },
+    ],
     labels: ['#00CE67', '#FFE347', '#ffa500'],
   });
 
@@ -35,13 +41,18 @@ const ChartModal: React.FC<ChartModalProps> = ({ isVisible, onClose }) => {
             data.push(i.id);
           }
           setChartData({
-            datasets: [{
-              data: data,
-              backgroundColor: ['#00CE67', '#ffa500', '#FFE347'],
-            }],
+            datasets: [
+              {
+                data: data,
+                backgroundColor: ['#00CE67', '#ffa500', '#FFE347'],
+              },
+              {
+                data: data.map((item) => item * 2),
+                backgroundColor: ['#D81E5B', '#731DD8', '#FAEDCA'],
+              },
+            ],
             labels: label,
           });
-          
         })
         .catch((e) => {
           console.log('Error fetching data:', e);
@@ -57,7 +68,17 @@ const ChartModal: React.FC<ChartModalProps> = ({ isVisible, onClose }) => {
         <button className={styles.closeButton} onClick={onClose}>
           &times;
         </button>
-        <Pie data={chartData} />
+        {/* First Pie chart */}
+        <h2>You owe</h2>
+        <div className={styles.chartContainer}>
+          <Pie data={{ datasets: [chartData.datasets[0]], labels: chartData.labels }} />
+        </div>
+        <br/>
+        {/* Second Pie chart */}
+        <h2>You are owed</h2>
+        <div className={styles.chartContainer}>
+          <Pie data={{ datasets: [chartData.datasets[1]], labels: chartData.labels }} />
+        </div>
       </div>
     </div>
   );
